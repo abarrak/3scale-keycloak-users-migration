1e7f736a78fbc44976f24a017eff0c7e87a4acf3
Abdullah Barrak <a@abarrak.com>
2021-12-29 00:25:41 +0300

    init gem

---
+++ lib/keycloak_3scale_users.rb
@@ -0,0 +1,6 @@
+require "keycloak_3scale_users/version"
+
+module Keycloak3scaleUsers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/keycloak_3scale_users/version.rb
@@ -0,0 +1,3 @@
+module Keycloak3scaleUsers
+  VERSION = "0.1.0"
+end
